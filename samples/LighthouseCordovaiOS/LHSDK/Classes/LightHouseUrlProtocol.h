394df4534bf059d9f8fd39165ced8e654246ca33
Kyle Champlin <k.champlin@gmail.com>
2015-07-08 09:30:55 -0700

    added a sample cordova project

---
+++ samples/LighthouseCordovaiOS/LHSDK/Classes/LightHouseUrlProtocol.h
@@ -0,0 +1,18 @@
+//
+//  LightHouseUrlProtocol.h
+//  HttpSSLClient
+//
+//  Created by Kyle Champlin on 5/6/15.
+//  Copyright (c) 2015 BusyWait. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MAPSDK.h"
+
+@interface LightHouseUrlProtocol : NSURLProtocol
+
+@property (nonatomic, strong) NSMutableData *mutableData;
+@property (nonatomic, strong) NSURLResponse *response;
+
+
+@end
